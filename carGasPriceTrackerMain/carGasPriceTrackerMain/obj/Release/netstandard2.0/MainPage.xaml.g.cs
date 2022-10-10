5af19455fb9b77c2964e7e98fc5977a105093603
deimantejagu <96204494+deimantejagu@users.noreply.github.com>
2022-10-10 23:43:35 +0300

    Added gitignore

--- carGasPriceTrackerMain/carGasPriceTrackerMain/obj/Release/netstandard2.0/MainPage.xaml.g.cs
+++ carGasPriceTrackerMain/carGasPriceTrackerMain/obj/Release/netstandard2.0/MainPage.xaml.g.cs
@@ -17,12 +17,12 @@ namespace carGasPriceTrackerMain {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
-        private global::Xamarin.Forms.ListView TodoList;
+        private global::Xamarin.Forms.ListView listviewConacts;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
-            TodoList = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "TodoList");
+            listviewConacts = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ListView>(this, "listviewConacts");
         }
     }
 }
